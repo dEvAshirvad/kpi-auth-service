10f8a47538e178a19147e1ddb3b67f3c048dba96
Ashirvad Satapathy <74089931+dEvAshirvad@users.noreply.github.com>
2025-07-30 12:52:52 +0530

    feat: departments api

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import membersRouter from './members/members.route';
+import departmentsRouter from './departments/departments.route';
 
 const router = express.Router();
 
 // Register routes
 router.use('/members', membersRouter);
+router.use('/departments', departmentsRouter);
 
 export default router;
